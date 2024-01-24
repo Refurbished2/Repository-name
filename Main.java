931fe014d7378559698ce5da6e47eac32dab8962
grify Sego <grifydev@gmail.com>
2024-01-23 18:24:11 -0800

    MAKW PROGRAM WORK

--- Main.java
+++ Main.java
@@ -22,6 +22,7 @@ public class Main {
 
     public static void main(String[] args){
 		while(true){
+			//pixel.paintComponent(null);
 			//System.out.println("\033[32m"+String.join("and",args));
 			System.out.print(randomize101());
 			System.out.print(randomize101());
